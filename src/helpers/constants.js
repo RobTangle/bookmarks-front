537aace882f148d243cec64e0e716252a810a3bc
Robert0101 <rob.tangled@gmail.com>
2023-01-10 01:54:27 -0300

    Crear CONST header

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,2 +1,9 @@
 export const URL_SERVER = "http://localhost:3333";
 export const NAME_ACCESS_TOKEN = "access_token_bookmarks";
+export function header(accessToken) {
+  return {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+}
